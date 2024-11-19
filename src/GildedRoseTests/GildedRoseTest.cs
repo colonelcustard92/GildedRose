65ac730b3e1d74d96ec4d947ce66dc00975a20d0
Reece Cottam <reece.cottam@speakersforschools.org>
2024-11-19 22:29:52 +0000

    Tests passing, need to implement new conjured item feature

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -52,12 +52,49 @@ namespace GildedRoseTests
         [Fact]
         public void Sulfuras_Never_Decreases_In_Quality_Nor_Has_To_Be_Sold()
         {
-            IList<Item> Items = new List<Item> { new Item { Name ="Sulfuras", SellIn = 20, Quality = 50 } };
+            IList<Item> Items = new List<Item> { new Item { Name ="Sulfuras", SellIn = 20, Quality = 80 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal(50,Items[0].Quality);
+            Assert.Equal(80,Items[0].Quality);
             Assert.Equal(20,Items[0].SellIn);
 
+        }
+
+        [Fact]
+        public void Backstage_Pass_Increases_In_Quality_By_2_Given_10_Or_Less_Days()
+        { 
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20} };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(22, Items[0].Quality);
+        }
+
+        [Fact]
+        public void Backstage_Pass_Increases_In_Quality_By_3_Given_5_Or_Less_Days()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(23, Items[0].Quality);
+        }
+
+        [Fact]
+        public void Backstage_Pass_Quality_Goes_To_0_If_Sellin_is_0()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 20 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void Conjured_Item_Degrades_Twice_As_Fast_As_A_Normal_Item()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Sausage", SellIn = 10, Quality = 18 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(9, Items[0].SellIn);
+            Assert.Equal(16, Items[0].Quality);
         }
 
     }

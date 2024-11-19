45842e0fec3ab3b7c5f2eb87214ee3dcd94679d1
Reece Cottam <reece.cottam@speakersforschools.org>
2024-11-19 21:56:00 +0000

    Added some quality tests, all good so far :)

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -6,13 +6,28 @@ namespace GildedRoseTests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
+        [Theory]
+        [InlineData(10,10)]
+        public void Aged_Brie_Increases_In_Quality_With_Age(int sellIn, int quality)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = sellIn, Quality = quality } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal(11, Items[0].Quality);
+            Assert.Equal(9, Items[0].SellIn);
+        }
+
+
+        [Theory]
+        [InlineData(0, 10)]
+        public void Given_Item_Is_Past_Sellby_Date_Quality_Degrades_By_Double(int sellIn, int quality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Bread of Doom", SellIn = sellIn, Quality = quality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal(8, Items[0].Quality);
+           
         }
     }
 }
+
